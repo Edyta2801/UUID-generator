8273c65b6febb0b5b658801a368126af5571527c
Edyta2801 <edyta.szarowska@interia.pl>
2020-10-28 15:35:52 +0100

    add hook useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { v4 as uuidv4 } from "uuid";
 
 function App() {
+
+  const [id, setId] = useState(uuidv4());
+  const handleClick = () => setId(uuidv4());
+
   return (
     <div className="App">
-     <p>{uuidv4()}</p>
+      <p>{id}</p>
+      <button onClick={handleClick}>Refresh me</button>
     </div>
   );
 }
